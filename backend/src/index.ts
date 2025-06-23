49ae6593217476fe656bf6d49def0870db7574ba
alongkorn <alongkornp126@gmail.com>
2025-06-23 15:51:55 +0700

    set up backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/v1/user", userRoutes);
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
